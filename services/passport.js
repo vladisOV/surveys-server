e9992552411b5408ce281e7f2e1efe2a9c6906d4
vladisov <vlad079@ya.ru>
2018-04-21 10:08:48 +0300

    user model + saving

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -9,10 +12,11 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback"
     },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
+    async (accessToken, refreshToken, profile, done) => {
+      const user = await User.findOne({ googleId: profile.id });
+      if (!user) {
+        await new User({ googleId: profile.id }).save();
+      }
     }
   )
 );
