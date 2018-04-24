a3d573fc445b0a02f9d47aa11c68ec4824556ca9
Vladislav.Ovcharenko <Vladislav.Ovcharenko@x5.ru>
2018-04-24 16:55:47 +0300

    some routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => {
+  return <h2>header</h2>;
+};
+const Dashboard = () => {
+  return <h2>Dashboard</h2>;
+};
+const Landing = () => {
+  return <h2>Landing</h2>;
+};
+const SurveyNew = () => {
+  return <h2>SurveyNew</h2>;
+};
 
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
