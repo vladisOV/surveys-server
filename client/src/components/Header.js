31435ef2c09b5183cd8bd43f08b4ac45712387b3
vladisov <vlad079@ya.ru>
2018-04-25 22:01:59 +0300

    header design

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,20 @@ import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="left brand-logo">
+            Surveys
+          </a>
+          <ul class="right">
+            <li>
+              <a>Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
