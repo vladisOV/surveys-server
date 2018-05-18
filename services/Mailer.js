be794b31ec45d8c63e73a282a02901f88ae489b8
vladisov <vlad079@ya.ru>
2018-05-18 20:11:48 +0300

    mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,4 +2,15 @@ const sendgrid = require("sendgrid");
 const helper = sendgrid.mail;
 const keys = require("../config/keys");
 
-class Mailer extends help.Mailer {}
+class Mailer extends help.Mailer {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email("no-reply@surveys.com");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAdresses(recipients);
+  }
+}
+
+module.exports = Mailer;
