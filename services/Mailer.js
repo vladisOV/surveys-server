27f35106c6bf59dbc5dbd7a1a29a9d90328ec8fa
vladisov <vlad079@ya.ru>
2018-05-21 21:46:00 +0300

    tracking settings

--- services/Mailer.js
+++ services/Mailer.js
@@ -10,12 +10,19 @@ class Mailer extends help.Mailer {
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
     this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
   }
 
   formatAddresses(recipients) {
     return recipients.map(({ email }) => {
       return new helper.Email(email);
     });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
   }
 }
 
