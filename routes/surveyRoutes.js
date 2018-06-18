148baac71df344f24c796815a3f2cebdad471ce1
vladisov <vlad079@ya.ru>
2018-06-18 22:10:54 +0300

    template polish

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,6 +7,10 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
+  app.get("api/surveys/thanks", (req, res) => {
+    res.send("Thanks for voting!");
+  });
+
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
@@ -22,6 +26,11 @@ module.exports = app => {
     const mailer = new Mailer(survey, surveyTemplate(survey));
     try {
       await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
     } catch (err) {
       res.status(422).send(err);
     }
